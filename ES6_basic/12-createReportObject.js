d8d69e3c1841efe972f5bab7e59e1aa6740159e3
German Silveira <adolfogsilveira@gmail.com>
2024-07-06 23:31:08 +0000

    Changes

---
+++ ES6_basic/12-createReportObject.js
@@ -0,0 +1,15 @@
+export default function createReportObject(elist) {
+  const departments = Object.keys(elist);
+
+  const allEmployees = {};
+  departments.forEach(department => {
+    allEmployees[department] = elist[department];
+  });
+
+  const getNumberOfDepartments = () => departments.length;
+
+  return {
+    allEmployees,
+    getNumberOfDepartments,
+  };
+}
