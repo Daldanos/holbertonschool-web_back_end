2900295b02f68e7edfd2c78767a4d21b6acbdc33
German Silveira <adolfogsilveira@gmail.com>
2024-07-07 22:23:32 +0000

    more changes

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -3,9 +3,7 @@ import { uploadPhoto, createUser } from './utils.js';
 async function handleProfileSignup() {
   try {
     const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
-
-    const photoId = typeof photo === 'object' ? photo.id : photo;
-
+    const photoId = (photo && typeof photo === 'object') ? photo.id : photo;
     console.log(`photo-profile-${photoId} ${user.firstName} ${user.lastName}`);
   } catch (error) {
     console.error("Signup system offline");
