3d345c1ac2c81fb7666af45f3d8898277b42ed6b
German Silveira <adolfogsilveira@gmail.com>
2024-07-07 21:29:46 +0000

    Changes

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,14 +1,12 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
-function handleProfileSignup() {
+export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
-    .then((values) => {
-      const [photo, user] = values;
+    .then((results) => {
+      const [photo, user] = results;
       console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
     });
 }
-
-export default handleProfileSignup;
