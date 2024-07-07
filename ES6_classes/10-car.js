bdf77760c8f2183377709fbc3f040368baec6896
German Silveira <adolfogsilveira@gmail.com>
2024-07-07 03:00:35 +0000

    changes

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,16 +1,11 @@
-// 10-car.js
-const brand = Symbol('brand');
-const motor = Symbol('motor');
-const color = Symbol('color');
-
 export default class Car {
     constructor(brand, motor, color) {
-        this[brand] = brand;
-        this[motor] = motor;
-        this[color] = color;
+        this._brand = brand;
+        this._motor = motor;
+        this._color = color;
     }
 
     cloneCar() {
-        return new Car(this[brand], this[motor], this[color]);
+        return new Car(this._brand, this._motor, this._color);
     }
 }
