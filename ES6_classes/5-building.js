6f7f15b64c993fe415b5ff348ded4a7ad00d209c
German Silveira <adolfogsilveira@gmail.com>
2024-07-07 01:53:40 +0000

    changes

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,26 +1,13 @@
 export default class Building {
   constructor(sqft) {
-    this._sqft = 0;
-    this.sqft = sqft;
+    this._sqft = sqft;
 
-    if (new.target === Building) {
-      throw new Error("Cannot instantiate an abstract class");
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  set sqft(newSqft) {
-    if (typeof newSqft === 'number') {
-      this._sqft = newSqft;
-    } else {
-      throw new TypeError('Square footage must be a number');
-    }
-  }
-
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
