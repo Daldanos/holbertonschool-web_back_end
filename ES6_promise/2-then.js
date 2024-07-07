dbe7ef7ba50caf1d7dca845632f31f2449ad7e2b
German Silveira <adolfogsilveira@gmail.com>
2024-07-07 18:12:18 +0000

    Changes

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,5 +1,5 @@
 function handleResponseFromAPI(promise) {
-  promise.then(() => {
+  return promise.then(() => {
     console.log('Got a response from the API');
     return {
       status: 200,
@@ -7,7 +7,7 @@ function handleResponseFromAPI(promise) {
     };
   }).catch(() => {
     console.log('Got a response from the API');
-    return new Error();
+    throw new Error();
   });
 }
 
