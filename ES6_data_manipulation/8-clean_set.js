b9e7525a9f519eebfb06c8d793d012e56d560169
German Silveira <adolfogsilveira@gmail.com>
2024-07-09 02:27:57 +0000

    changes

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -2,10 +2,9 @@ export default function cleanSet(set, startString) {
   const filteredValues = [];
 
   set.forEach(value => {
-    if (value.startsWith(startString)) {
+    if (startString && value.startsWith(startString)) {
       filteredValues.push(value.slice(startString.length));
     }
   });
-
   return filteredValues.join('-');
 }
